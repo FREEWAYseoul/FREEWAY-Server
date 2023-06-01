f9a31228de4b3343797b4c183a88625c9e3bdcb5
jcw1031 <jcw001031@gmail.com>
2023-06-01 20:13:48 +0900

    initial commit

---
+++ src/main/java/team/free/freeway/FreeWayApplication.java
@@ -0,0 +1,13 @@
+package team.free.freeway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeWayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/free/freeway/FreeWayApplicationTests.java
@@ -0,0 +1,13 @@
+package team.free.freeway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
