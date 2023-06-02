1625927af5361f2ceb34c0bb5346aa65e3858e67
jcw1031 <jcw001031@gmail.com>
2023-06-02 17:17:14 +0900

    Excel 리더 추가

---
+++ src/main/java/team/free/freeway/init/util/ExcelReader.java
@@ -0,0 +1,20 @@
+package team.free.freeway.init.util;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.stereotype.Component;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Component
+public class ExcelReader {
+
+    public Sheet readSheet(String filePath) throws IOException {
+        FileInputStream file = new FileInputStream(filePath);
+        Workbook workbook = new XSSFWorkbook(file);
+
+        return workbook.getSheetAt(0);
+    }
+}
