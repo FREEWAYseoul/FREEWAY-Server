5c4d2c53f87228daf978348690f010b5a2dfb89c
jcw1031 <jcw001031@gmail.com>
2023-08-10 15:45:00 +0900

    fix: Configuration 설정

--- src/main/java/team/free/freeway/config/WebMvcConfig.java
+++ src/main/java/team/free/freeway/config/WebMvcConfig.java
@@ -1,18 +1,16 @@
 package team.free.freeway.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("freewaykr.s3-website.ap-northeast-2.amazonaws.com")
                 .allowedOrigins("http://freewaykr.s3-website.ap-northeast-2.amazonaws.com")
-                .allowedOrigins("https://freewaykr.s3-website.ap-northeast-2.amazonaws.com")
-                .allowedOrigins("localhost:3000")
-                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET");
     }
 }
