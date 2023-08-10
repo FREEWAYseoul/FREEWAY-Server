9b54bb82e54ef2f923ab13d48d37744d3ab0496e
jcw1031 <jcw001031@gmail.com>
2023-08-10 15:44:08 +0900

    fix: CORS 재설정

--- src/main/java/team/free/freeway/config/WebMvcConfig.java
+++ src/main/java/team/free/freeway/config/WebMvcConfig.java
@@ -8,6 +8,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://freewaykr.s3-website.ap-northeast-2.amazonaws.com");
+                .allowedOrigins("freewaykr.s3-website.ap-northeast-2.amazonaws.com")
+                .allowedOrigins("http://freewaykr.s3-website.ap-northeast-2.amazonaws.com")
+                .allowedOrigins("https://freewaykr.s3-website.ap-northeast-2.amazonaws.com")
+                .allowedOrigins("localhost:3000")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET");
     }
 }
